2d9d31aa80e6bb531fd5fce55ff3413ea273b666
Daniel Wennström <daniel@wennstrom.io>
2025-03-19 13:52:29 +0100

    CRUD methods for CountryLanguageDaoImpl

--- src/main/java/se/lexicon/dao/CountryLanguageDaoImpl.java
+++ src/main/java/se/lexicon/dao/CountryLanguageDaoImpl.java
@@ -1,12 +1,14 @@
 package se.lexicon.dao;
 
-import se.lexicon.model.City;
+import se.lexicon.db.DBConnection;
 import se.lexicon.model.CountryLanguage;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class CountryLanguageDaoImpl implements CountryLanguageDao {
     private CountryLanguage mapRow(ResultSet resultSet) throws SQLException {
@@ -20,31 +22,121 @@ public class CountryLanguageDaoImpl implements CountryLanguageDao {
 
     @Override
     public CountryLanguage findByCode(String code) throws SQLException {
-        return null;
+        String query = "SELECT * FROM countrylanguage WHERE CountryCode = ?";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, code);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return mapRow(resultSet);
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
     public List<CountryLanguage> findByLanguageName(String name) throws SQLException {
-        return List.of();
+        String query = "SELECT * FROM countrylanguage WHERE Language = ?";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<CountryLanguage> languages = new ArrayList<>();
+
+            while (resultSet.next()) {
+                CountryLanguage language = mapRow(resultSet);
+                languages.add(language);
+            }
+
+            return languages;
+        }
     }
 
     @Override
     public List<CountryLanguage> findAll() throws SQLException {
-        return List.of();
+        String query = "SELECT * FROM countrylanguage";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<CountryLanguage> languages = new ArrayList<>();
+
+            while (resultSet.next()) {
+                CountryLanguage language = mapRow(resultSet);
+                languages.add(language);
+            }
+
+            return languages;
+        }
     }
 
     @Override
     public CountryLanguage save(CountryLanguage language) throws SQLException {
-        return null;
+        String query = "INSERT INTO countrylanguage (CountryCode, Language, IsOfficial, Percentage)" +
+                " VALUES (?, ?, ?, ?)";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, language.getCountryCode());
+            preparedStatement.setString(2, language.getLanguage());
+            preparedStatement.setBoolean(3, language.isOfficial());
+            preparedStatement.setDouble(4, language.getPercentage());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Language saved successfully");
+            } else {
+                System.out.println("Failed to save Language");
+            }
+
+            return language;
+        }
     }
 
     @Override
     public void update(CountryLanguage language) throws SQLException {
+        String query = "UPDATE countrylanguage SET CountryCode = ?," +
+                " Language = ?, IsOfficial = ?, Percentage = ? WHERE CountryCode = ?";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
 
+            preparedStatement.setString(1, language.getCountryCode());
+            preparedStatement.setString(2, language.getLanguage());
+            preparedStatement.setBoolean(3, language.isOfficial());
+            preparedStatement.setDouble(4, language.getPercentage());
+            preparedStatement.setString(5, language.getCountryCode());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Language updated successfully");
+            } else {
+                System.out.println("Failed to update Language");
+            }
+        }
     }
 
     @Override
     public void deleteByCode(String countryCode) throws SQLException {
+        String query = "DELETE FROM countrylanguage WHERE CountryCode = ?";
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
 
+            preparedStatement.setString(1, countryCode);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Language deleted successfully");
+            } else
+                System.out.println("Failed to delete Language");
+        }
     }
 }
