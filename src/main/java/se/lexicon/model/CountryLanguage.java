306173b18026a4e48e164ef9c83d4eb51cdffcf0
Daniel Wennström <daniel@wennstrom.io>
2025-03-19 12:56:01 +0100

    G&S and constructors for Country and CountryLanguage

--- src/main/java/se/lexicon/model/CountryLanguage.java
+++ src/main/java/se/lexicon/model/CountryLanguage.java
@@ -1,4 +1,53 @@
 package se.lexicon.model;
 
 public class CountryLanguage {
+    private int id;
+    private String countryCode;
+    private String language;
+    private boolean isOfficial;
+
+    public CountryLanguage(String countryCode, String language, boolean isOfficial, int id) {
+        this.countryCode = countryCode;
+        this.language = language;
+        this.isOfficial = isOfficial;
+        this.id = id;
+    }
+
+    public CountryLanguage(String countryCode, String language, boolean isOfficial) {
+        this.countryCode = countryCode;
+        this.language = language;
+        this.isOfficial = isOfficial;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public boolean isOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(boolean official) {
+        isOfficial = official;
+    }
 }
