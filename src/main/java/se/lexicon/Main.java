5fe8ad9f489462dd8f44bf1ba3363143e2c1b076
Daniel Wennström <daniel@wennstrom.io>
2025-03-19 09:54:10 +0100

    Initial main program implementation

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,10 +1,35 @@
 package se.lexicon;
 
+import java.util.Scanner;
+
 /**
  * Represents the entry point of the application.
  */
 public class Main {
     public static void main(String[] args) {
-        // TODO: Needs completion
+        boolean running = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (running) {
+            System.out.println("Welcome to CRUDe World!");
+            System.out.println("Please select an option (or q to exit):");
+            switch (scanner.nextLine()) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "q":
+                    running = false;
+                    System.out.println("Thank you for using CRUDe World!");
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
     }
 }
